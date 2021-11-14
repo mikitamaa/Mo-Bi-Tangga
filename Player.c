9f0375b4b008314936005ccea522f214bc8abb89
Rey Shazni <69889187+reyshazni@users.noreply.github.com>
2021-11-14 16:48:40 +0700

    Add files via upload

---
+++ Player.c
@@ -0,0 +1,143 @@
+#include "player.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void createEmptyPlayerList(pUserName *pU)
+{
+    (*pU).Neff = 0;
+}
+
+void createEmptyPlayerSkillsList(Skill *S)
+{
+    ADDR_HEADSKILL(*S) = Nil;
+}
+
+address newSkillNode()
+{
+    lsNode *P = (lsNode *)malloc(sizeof(lsNode));
+    if (P != Nil)
+    {
+        NEXTSKILL(P) = Nil;
+        strcpy(SKILLNAME(P), "kosong");
+        return P;
+    }
+    else
+    {
+        return Nil;
+    }
+}
+
+void preparationSkillList(Skill *pS1, Skill *pS2, Skill *pS3, Skill *pS4, int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1)
+        {
+            createEmptyPlayerSkillsList(pS1);
+        }
+        else if (i == 2)
+        {
+            createEmptyPlayerSkillsList(pS2);
+        }
+        else if (i == 3)
+        {
+            createEmptyPlayerSkillsList(pS3);
+        }
+        else if (i == 4)
+        {
+            createEmptyPlayerSkillsList(pS4);
+        }
+    }
+}
+
+void summonPlayer(pUserName *pU, pIsTeleported *pT, pPosition *pP, pIsImune *pI, int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printf("coba masukin uname\n");
+        scanf("%s", &((*pU).uname[i]));
+        (*pT).isTele[i] = FALSE;
+        (*pP).pos[i] = 1;
+        (*pI).isImun[i] = FALSE;
+    }
+    (*pU).Neff = n;
+}
+
+int getIdxOfPlayer(pUserName pU, char *name)
+{
+    int i = IdxMin;
+    char temp[16];
+    strcpy(temp, name);
+    while ((strcmp(pU.uname[i], temp) != 0) && (i <= IdxMax))
+    {
+        i++;
+    }
+    if (i <= IdxMax)
+    {
+        return i;
+    }
+    else
+    {
+        return IdxUndef;
+    }
+}
+
+boolean isEmptyList(Skill pS)
+{
+    return ((ADDR_HEADSKILL(pS)) == Nil);
+}
+
+void insertVSkill(Skill *pS, char *skname)
+{
+    if (isEmptyList(*pS))
+    {
+        address P = newSkillNode();
+        strcpy(SKILLNAME(P), skname);
+        NEXTSKILL(P) = Nil;
+        ADDR_HEADSKILL(*pS) = P;
+    }
+    else
+    {
+        address P = ADDR_HEADSKILL(*pS);
+        int count = 1;
+        while ((NEXTSKILL(P) != Nil) && (count <= 10))
+        {
+            count++;
+            P = NEXTSKILL(P);
+        }
+        if (count <= 10)
+        {
+            address X = newSkillNode();
+            strcpy(SKILLNAME(X), skname);
+            NEXTSKILL(X) = Nil;
+            NEXTSKILL(P) = X;
+        }
+    }
+}
+
+boolean getTeleportedConditionOfPlayer(pIsTeleported pT, pUserName pU, char *uname)
+{
+    int idx = getIdxOfPlayer(pU, uname);
+    if (idx != IdxUndef)
+    {
+        return (pT.isTele[idx]);
+    }
+}
+
+boolean getImmunityConditionOfPlayer(pIsImune pI, pUserName pU, char *uname)
+{
+    int idx = getIdxOfPlayer(pU, uname);
+    if (idx != IdxUndef)
+    {
+        return (pI.isImun[idx]);
+    }
+}
+
+int getPositionOfPlayer(pPosition pP, pUserName pU, char *uname)
+{
+    int idx = getIdxOfPlayer(pU, uname);
+    if (idx != IdxUndef)
+    {
+        return (pP.pos[idx]);
+    }
+}
