ee754b7adce8c0f34c2be292750ba8b422d8bf8f
Rey Shazni <69889187+reyshazni@users.noreply.github.com>
2021-11-20 20:43:58 +0700

    mencocokkan dgn adt player

--- roll.h
+++ roll.h
@@ -2,9 +2,9 @@
 #define __ROLL_H__
 
 #include "map.h"
-#include "player.h"
+#include "Player.h"
 
-int HasilRoll(MAP Map, Player Player, int i) ;
+int HasilRoll(MAP Map, Player P, int i);
 /* 
 Fungsi untuk mendapatkan hasil dari roll
 */
