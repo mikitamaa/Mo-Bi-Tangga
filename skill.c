b4b5cb8efb1fd50cc0e67fa7916de46913a58e1e
mikitamaa <73424893+mikitamaa@users.noreply.github.com>
2021-11-21 01:40:13 +0700

    updated skill.c udah bisa yey

--- skill.c
+++ skill.c
@@ -1,5 +1,7 @@
 #include "Player.h"
+#include "Player.c"
 #include "skill.h"
+#include <stdio.h>
 
 /* 
 Identifier Skill
@@ -11,44 +13,93 @@ Identifier Skill
 6 = Teknologi Gagal
  */
 
-boolean isEmptyList(Lskill *pS){
+// ----------------------------------------------------------------------- Generic Purpose Use ----------------------------------------------------------------------------------- //
+address newSkillNode(){
 /*
- Mereturn True jika pS kosong
+ Fungsi mereturn sebuah address dari Skill (list skill node)
+ Jika fungsi gagal mengalokasi Skill maka akan mereturn Nil
 */
-    return (FirstSKILL(pS) == Nil);
-}
-
-Skill draw(){
-
-}
-
-
-void pintuGKM(pIsImune *I, int P){
-/* Player mendapat buff imunitas dari teleporter */
-/* tidak mendapat ekstra ketika diaktifkan berulang kali */
-    if (I->isImun[P] != true)
+    address S = (address)malloc(sizeof(Skill));
+    if (S != Nil)
     {
-        I->isImun[P] = true;
+        Id(S) = 0;
+        Next(S) = Nil;
+
+        return S;
+    }
+    else {
+        return Nil;
     }
 }
 
 
-void cerminPengganda(Lskill *LS){
-/* Menjalankan prosedur draw sebanyak 2 kali dan menambahkan skill yang didapat ke pemain */
-    Skill S1, S2;
-    S1 = draw();
-    S2 = draw();
-    insertVSkill(LS, S1);
-    insertVSkill(LS, S2);
+void CreateEmpty (lSkill *lS) {
+/* I.S. sembarang             */
+/* F.S. Terbentuk list kosong */
+    *lS = Nil;
 }
 
 
-void mesinPenukarPosisi(pPosition *p, int pA, int pB){
+void insertSkill (lSkill *lS, address S){
+    address last;
+
+    if (*lS == Nil)
+    {
+        *lS = S;
+        *lS = S;
+    }
+
+    else {
+        last = *lS;
+        while (Next(last) != Nil)
+        {
+            last = Next(last);
+        }
+        last = S;
+        *lS = S;
+    }
+    
+}
+
+
+void draw(Player *P, int currentPlayer){
+    address S;
+    S = newSkillNode();
+    Id(S) = 1;
+    insertSkill(&skills(*P)[currentPlayer], S);
+}
+
+
+// ----------------------------------------------------------------------- Skill Effects ----------------------------------------------------------------------------------- //
+void pintuGKM(Player *P, int currentPlayer){
+/* Player mendapat buff imunitas dari teleporter */
+/* tidak mendapat ekstra ketika diaktifkan berulang kali */
+    if (isImmu(*P)[currentPlayer] == false)
+    {
+        isImmu(*P)[currentPlayer] = true;
+    }
+}
+
+
+void cerminPengganda(Player *P, int currentPlayer){
+/* Menjalankan prosedur draw sebanyak 2 kali dan menambahkan skill yang didapat ke pemain */
+    if (!isCermin(*P)[currentPlayer])
+    {
+        draw(&P, currentPlayer);
+        draw(&P, currentPlayer);
+    } 
+}
+
+
+void mesinPenukarPosisi(Player *P, int currentPlayer){
 /* Menukarkan posisi dua pemain pada map. */
 /* tidak mengaktifkan teleporter. */
-    ElType temp = &p->pos[pB];
-    p->pos[pB] = p->pos[pA];
-    p->pos[pA] = temp;
+    ElType temp;
+    int targetedPlayer;
+    printf("Masukkan nomor player yang ingin ditarget: ");
+    scanf("%d", &targetedPlayer);
+    temp = pos(*P)[targetedPlayer];
+    pos(*P)[targetedPlayer] = pos(*P)[currentPlayer];
+    pos(*P)[currentPlayer] = temp;
+
 }
-
-
